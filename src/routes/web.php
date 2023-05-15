5b28d2f630b01d0c9a318c996f260e7feee1fee4
WinTDemi <thangmai12082001@gmail.com>
2023-05-15 17:04:06 +0700

    route

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,46 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/single/{one}', function ($one) {
+    //truyền một parameter
+    return "$one";
+});
+
+Route::get('/double/{one}/{two}', function ($one, $two) {
+    //truyền hai parameter
+    return "$one & $two";
+});
+
+Route::get('login/{username?}/{password?}', function ($username=null, $password=null) {
+    //parameter không bắt buộc thì bạn chỉ việc thêm dấu
+    if (!$username) {
+        return "Xin mời nhập tên đăng nhập";
+    }
+    if (!$password) {
+        return "Xin mời nhập mật khẩu";
+    }
+    else {
+        return "tài khoải: $username <br> mật khẩu: $password";
+    }
+
+});
+
+Route::get('/test-name/{name}/{var}', function ($name, $var) {
+    //gọi parameter name
+    if($name == 'where-string') $varName = 'string';
+    if($name == 'where-number') $varName = 'number';
+    return redirect()->route("test.$name", ["$varName" => "$var"]);
+});
+
+Route::prefix('test')->name('test.')->group(function () {
+    Route::get('/where-number/{number}', function ($number) {
+        //parameter number
+        return "$number";
+    })->whereNumber('number')->name('where-number');
+    
+    Route::get('/where-string/{string}', function ($string) {
+        //parameter string
+        return "$string";
+    })->whereAlpha('string')->name('where-string');
+});
